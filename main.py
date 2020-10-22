d9bb66c9eb6af113178de37b12b9b4c3b425f86a
Rybalochka-MS <medickus97@gmail.com>
2020-10-22 15:16:36 +0300

    Host changes

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from bottle import run
 from src.server import *
 
 if __name__ == '__main__':
-    run(host='ec2-3-122-158-250.eu-central-1.compute.amazonaws.com', port=80, debug=False)
+    run(host='upa.testawsfuture.com', port=80, debug=False)
