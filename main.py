ed49ca6652a24e3cb3eb267ea68329ef0fef08a3
Rybalochka-MS <medickus97@gmail.com>
2020-12-04 18:52:03 +0200

    Add authomatic replace host & minor bugfix

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from bottle import run
 from src.server import *
+from src.load_properties import get_host
+from src.replace_url import replace_url
 
 if __name__ == '__main__':
-    run(host='upa.testawsfuture.com', port=80, debug=False)
+    replace_url()
+    hst = get_host()
+    run(host=hst, port=80, debug=False)
--- src/load_properties.py
+++ src/load_properties.py
@@ -13,3 +13,18 @@ def get_db_credentials():
     user = config.get('DataBase', 'database.user')
     password = config.get('DataBase', 'database.password')
     return host, user, password
+
+
+def get_url():
+    config = load_config()
+    protocol = 'http://'
+    host = config.get('Server', 'server.host')
+    port = ':80'
+    url = protocol + host + port
+    return url
+
+
+def get_host():
+    config = load_config()
+    host = config.get('Server', 'server.host')
+    return host
